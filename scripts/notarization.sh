20e48c3f704cfc6fd503ee25917770d028e7e451
milan <m.stefanovic@tum.de>
2021-06-11 01:50:04 +0200

    script notarization

--- scripts/notarization.sh
+++ scripts/notarization.sh
@@ -1,3 +1,5 @@
+set +x
+
 cd ~/caliper-benchmarks/src/masterCC/notarization && npm install
 cd ~/caliper-benchmarks/src/masterCC/notarization/lib && node generator.js
 
@@ -6,3 +8,5 @@ cd ~/caliper-benchmarks/benchmarks/masterBenchmark/notarization/ && npm install
 cp ~/caliper-benchmarks/src/masterCC/notarization/lib/seeds.json ~/caliper-benchmarks/benchmarks/masterBenchmark/notarization/
 
 npx caliper launch master --caliper-workspace . --caliper-benchconfig benchmarks/masterBenchmark/notarization/config.yaml --caliper-networkconfig networks/masterScripts/fabric-node-leveldb.yaml 
+
+set -x
