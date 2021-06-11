43d1e38fc095c5beac5c3cc23ee33c52aa923c63
milan <m.stefanovic@tum.de>
2021-06-11 18:54:26 +0200

    fix script

--- scripts/eureka.sh
+++ scripts/eureka.sh
@@ -1,12 +1,12 @@
 set -x
 
-cd ~/caliper-benchmarks/src/masterCC/eureka && npm install
-cd ~/caliper-benchmarks/src/masterCC/eureka/lib && node generator.js
+cd ~/caliper-benchmarks/src/masterCC/eureka/node && npm install
+cd ~/caliper-benchmarks/src/masterCC/eureka/node/lib && node generator.js
 
 cd ~/caliper-benchmarks/benchmarks/masterBenchmark/eureka/ && npm install
 
-cp ~/caliper-benchmarks/src/masterCC/eureka/lib/seeds.json ~/caliper-benchmarks/benchmarks/masterBenchmark/eureka/
+cp ~/caliper-benchmarks/src/masterCC/eureka/node/lib/seeds.json ~/caliper-benchmarks/benchmarks/masterBenchmark/eureka/
 
-npx caliper launch master --caliper-workspace . --caliper-benchconfig benchmarks/masterBenchmark/eureka/config.yaml --caliper-networkconfig networks/masterScripts/fabric-node-couchdb.yaml 
+npx caliper launch master --caliper-workspace . --caliper-benchconfig benchmarks/masterBenchmark/eureka/config.yaml --caliper-networkconfig networks/masterScripts/fabric-couchdb.yaml 
 
 set +x
