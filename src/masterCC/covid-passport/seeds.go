eea281fc84fd536a1fd64f002b20a3f85ff1de89
milan <m.stefanovic@tum.de>
2021-06-11 23:47:35 +0200

    test

--- src/masterCC/covid-passport/seeds.go
+++ src/masterCC/covid-passport/seeds.go
@@ -6,11 +6,7 @@ import (
 	"io/ioutil"
 	"math/rand"
 	"net/http"
-	"path"
-	"strconv"
 	"time"
-
-	"gopkg.in/yaml.v2"
 )
 
 var (
@@ -62,67 +58,4 @@ func loadFromUrl(url string) ([]byte, error) {
 		return nil, fmt.Errorf("Error loading URL: %w", err)
 	}
 	return ioutil.ReadAll(r.Body)
-}
-
-func generateSeeds() error {
-	// Seed random gen
-	rand.Seed(time.Now().UnixNano())
-
-	// Read seed params
-	seedParamsB, err := ioutil.ReadFile(path.Join("hack", "seed", "seedParameters.yaml"))
-	if err != nil {
-		return fmt.Errorf("Error reading seedParameters.yaml: %w", err)
-	}
-	var seedParams SeedParameters
-	if err := yaml.Unmarshal(seedParamsB, &seedParams); err != nil {
-		return fmt.Errorf("Error unmarshaling seedParameters.yaml: %w", err)
-	}
-
-	// Initialize Seeds
-	seeds := Seeds{}
-
-	// Generate Valid DHPs
-	for i := 0; i < seedParams.AllValidDhp; i++ {
-		dhp, err := generateValidDhp(i)
-		if err != nil {
-			return fmt.Errorf("Error generating Valid DHP #%d: %w", i, err)
-		}
-		seeds.ValidDhps = append(seeds.ValidDhps, *dhp)
-	}
-
-	// Store seeds.json
-	seedsJson, err := json.Marshal(&seeds)
-	if err != nil {
-		return fmt.Errorf("Error marshaling generated seeds: %w", err)
-	}
-	if err := ioutil.WriteFile(path.Join("hack", "seed", "seeds.json"), seedsJson, 0644); err != nil {
-		return fmt.Errorf("Error writing seeds.json: %w", err)
-	}
-
-	return nil
-}
-
-func generateValidDhp(ix int) (*Dhp, error) {
-	var tfId string
-	r := rand.Intn(len(TfKeys))
-	for k := range TfKeys {
-		if r == 0 {
-			tfId = k
-		}
-		r--
-	}
-	tfKey, err := UnmarshalPrivateKey(TfKeys[tfId])
-	if err != nil {
-		return nil, fmt.Errorf("Error unmarshaling private key: %w", err)
-	}
-	patientDid := PatientDids[rand.Intn(len(PatientDids))]
-
-	// fmt.Printf("PatientDID: %s", patientDid)
-	// fmt.Printf("SHA256(PatientDID: %s", (ccrypto.Hash([]byte(patientDid))))
-
-	return GenerateDhp(strconv.Itoa(ix), tfId, tfKey, patientDid,
-		TestTypes[rand.Intn(len(TestTypes))], rand.Intn(1) == 1,
-		time.Now().AddDate(0, 0, -1).Truncate(24*time.Hour),
-		time.Now().AddDate(0, 0, 2).Truncate(24*time.Hour),
-	)
 }
--- src/masterCC/covid-passport/seeds_test.go
+++ src/masterCC/covid-passport/seeds_test.go
@@ -1,7 +1,16 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"path"
+	"strconv"
 	"testing"
+	"time"
+
+	"gopkg.in/yaml.v2"
 )
 
 func Test_LoadSeeds(t *testing.T) {
@@ -18,4 +27,67 @@ func Test_GenerateSeeds(t *testing.T) {
 	if err := generateSeeds(); err != nil {
 		t.Fatal(err)
 	}
+}
+
+func generateSeeds() error {
+	// Seed random gen
+	rand.Seed(time.Now().UnixNano())
+
+	// Read seed params
+	seedParamsB, err := ioutil.ReadFile(path.Join("hack", "seed", "seedParameters.yaml"))
+	if err != nil {
+		return fmt.Errorf("Error reading seedParameters.yaml: %w", err)
+	}
+	var seedParams SeedParameters
+	if err := yaml.Unmarshal(seedParamsB, &seedParams); err != nil {
+		return fmt.Errorf("Error unmarshaling seedParameters.yaml: %w", err)
+	}
+
+	// Initialize Seeds
+	seeds := Seeds{}
+
+	// Generate Valid DHPs
+	for i := 0; i < seedParams.AllValidDhp; i++ {
+		dhp, err := generateValidDhp(i)
+		if err != nil {
+			return fmt.Errorf("Error generating Valid DHP #%d: %w", i, err)
+		}
+		seeds.ValidDhps = append(seeds.ValidDhps, *dhp)
+	}
+
+	// Store seeds.json
+	seedsJson, err := json.Marshal(&seeds)
+	if err != nil {
+		return fmt.Errorf("Error marshaling generated seeds: %w", err)
+	}
+	if err := ioutil.WriteFile(path.Join("hack", "seed", "seeds.json"), seedsJson, 0644); err != nil {
+		return fmt.Errorf("Error writing seeds.json: %w", err)
+	}
+
+	return nil
+}
+
+func generateValidDhp(ix int) (*Dhp, error) {
+	var tfId string
+	r := rand.Intn(len(TfKeys))
+	for k := range TfKeys {
+		if r == 0 {
+			tfId = k
+		}
+		r--
+	}
+	tfKey, err := UnmarshalPrivateKey(TfKeys[tfId])
+	if err != nil {
+		return nil, fmt.Errorf("Error unmarshaling private key: %w", err)
+	}
+	patientDid := PatientDids[rand.Intn(len(PatientDids))]
+
+	// fmt.Printf("PatientDID: %s", patientDid)
+	// fmt.Printf("SHA256(PatientDID: %s", (ccrypto.Hash([]byte(patientDid))))
+
+	return GenerateDhp(strconv.Itoa(ix), tfId, tfKey, patientDid,
+		TestTypes[rand.Intn(len(TestTypes))], rand.Intn(1) == 1,
+		time.Now().AddDate(0, 0, -1).Truncate(24*time.Hour),
+		time.Now().AddDate(0, 0, 2).Truncate(24*time.Hour),
+	)
 }
