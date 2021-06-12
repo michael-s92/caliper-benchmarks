c4077f5262743b5cb6cc82c14ff419e6d242c6e4
milan <m.stefanovic@tum.de>
2021-06-12 14:10:25 +0200

    java enum added

---
+++ src/masterCC/iotsupplychain/java/src/main/java/org/hyperledger/fabric/samples/fabcar/CCErrors.java
@@ -0,0 +1,11 @@
+package org.hyperledger.fabric.samples.fabcar;
+
+public enum CCErrors {
+
+	KEY_NULL_OR_EMPTY,
+	STATE_ALREADY_EXISTS,
+	STATE_DOES_NOT_EXIST,
+	PERCENT_OUT_OF_RANGE,
+	ATTRIBUT_ALREADY_POPULATED
+	
+}
