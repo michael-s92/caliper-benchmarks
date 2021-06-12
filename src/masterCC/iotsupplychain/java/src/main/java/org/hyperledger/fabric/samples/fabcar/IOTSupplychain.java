82a1b2c3ab3caf19b4e713c4f91b5f651052aace
milan <m.stefanovic@tum.de>
2021-06-12 14:03:24 +0200

    empty java test

---
+++ src/masterCC/iotsupplychain/java/src/main/java/org/hyperledger/fabric/samples/fabcar/IOTSupplychain.java
@@ -0,0 +1,62 @@
+/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package org.hyperledger.fabric.samples.fabcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hyperledger.fabric.contract.Context;
+import org.hyperledger.fabric.contract.ContractInterface;
+import org.hyperledger.fabric.contract.annotation.Contact;
+import org.hyperledger.fabric.contract.annotation.Contract;
+import org.hyperledger.fabric.contract.annotation.Default;
+import org.hyperledger.fabric.contract.annotation.Info;
+import org.hyperledger.fabric.contract.annotation.License;
+import org.hyperledger.fabric.contract.annotation.Transaction;
+import org.hyperledger.fabric.shim.ChaincodeException;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+import org.hyperledger.fabric.shim.ledger.KeyValue;
+import org.hyperledger.fabric.shim.ledger.QueryResultsIterator;
+import org.hyperledger.fabric.shim.ledger.QueryResultsIteratorWithMetadata;
+
+import com.owlike.genson.Genson;
+
+/**
+ * Java implementation of the Fabric Car Contract described in the Writing Your
+ * First Application tutorial
+ */
+@Contract(name = "iotsupplychain")
+@Default
+public final class IOTSupplychain implements ContractInterface {
+
+    private final Genson genson = new Genson();
+
+    @Transaction()
+    public void init(final Context ctx) {
+        // comment
+    }
+    
+    @Transaction()
+    public void doNothing(final Context ctx) {
+        // comment
+    }
+    
+    @Transaction()
+    public void initLedger(final Context ctx) {
+        // comment
+    }
+
+    
+}
