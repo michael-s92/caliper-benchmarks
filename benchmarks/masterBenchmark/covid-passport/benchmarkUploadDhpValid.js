fb76b79b93c6bde7dbd88bb4b9364b3c371e1ee2
milan <m.stefanovic@tum.de>
2021-06-14 00:31:49 +0200

    test covid

---
+++ benchmarks/masterBenchmark/covid-passport/benchmarkUploadDhpValid.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+'use strict';
+
+class benchmarkUploadDhpValid {
+
+    static get() {
+
+        let d = new Date();
+        let a = d.getHours() + d.getMinutes() + d.getSeconds();
+
+        // PurgeExpiredDhps()
+	    let args = {
+                chaincodeFunction: 'benchmarkUploadDhpValid',
+                chaincodeArguments: [a]
+            };
+
+	    return args;
+
+	}
+}
+
+module.exports = benchmarkUploadDhpValid;
