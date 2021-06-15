ee18dd2ce3de5a0162c6b68dff065675de22d3d1
Jelena Pajic <jelenapajic7@gmail.com>
2021-06-15 06:35:19 +0200

    explicitly add BigInteger converter to genson

--- src/masterCC/covid-passport-java/src/main/java/org/hyperledger/fabric/samples/fabcar/JsonConverters.java
+++ src/masterCC/covid-passport-java/src/main/java/org/hyperledger/fabric/samples/fabcar/JsonConverters.java
@@ -2,6 +2,7 @@ package org.hyperledger.fabric.samples.fabcar;
 
 import com.owlike.genson.Context;
 import com.owlike.genson.Converter;
+import com.owlike.genson.convert.DefaultConverters;
 import com.owlike.genson.Genson;
 import com.owlike.genson.GensonBuilder;
 import com.owlike.genson.stream.ObjectReader;
@@ -9,7 +10,10 @@ import com.owlike.genson.stream.ObjectWriter;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 
+import java.math.BigInteger;
+
 public class JsonConverters {
+    public static Converter<BigInteger> BIG_INTEGER = DefaultConverters.BigIntegerConverter.instance;
     public static Converter<DateTime> DATE_TIME = new Converter<DateTime>() {
         @Override
         public void serialize(DateTime obj, ObjectWriter writer, Context ctx) throws Exception {
@@ -29,6 +33,7 @@ public class JsonConverters {
     public static Genson Genson() {
         return new GensonBuilder()
                 .setSkipNull(true)
+                .withConverter(JsonConverters.BIG_INTEGER, java.math.BigInteger.class)
                 .withConverter(JsonConverters.DATE_TIME, org.joda.time.DateTime.class)
                 .create();
     }
